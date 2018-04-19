a0e6dfd810fc3fd5d08e79d4ce934a4dcdfb9c42
gauravprwl14 <gauravprwl14@gmail.com>
2018-04-19 15:45:28 +0530

    added actionSheet with regular and icons component

---
+++ src/screens/actionSheet/icon.vue
@@ -0,0 +1,63 @@
+<template>
+  <nb-container :style="{ backgroundColor: '#FBFAFA' }">
+        <nb-header>
+          <nb-left>
+            <nb-button
+              transparent
+              :onPress="() => this.props.navigation.goBack()"
+            >
+              <nb-icon name="arrow-back" />
+            </nb-button>
+          </nb-left>
+          <nb-body>
+            <nb-title>ActionSheet</nb-title>
+          </nb-body>
+          <nb-right />
+        </nb-header>
+        <nb-content padder>
+          <nb-button
+            :onPress="handleBtnPress"
+          >
+            <nb-text>Actionsheet</nb-text>
+          </nb-button>
+        </nb-content>
+      </nb-container>
+</template>
+
+
+<script>
+import { ActionSheet } from "native-base";
+
+export default {
+  data: function() {
+    return {
+      btnOptions: [
+        { text: "Option 0", icon: "american-football", iconColor: "#2c8ef4" },
+        { text: "Option 1", icon: "analytics", iconColor: "#f42ced" },
+        { text: "Option 2", icon: "aperture", iconColor: "#ea943b" },
+        { text: "Delete", icon: "trash", iconColor: "#fa213b" },
+        { text: "Cancel", icon: "close", iconColor: "#25de5b" }
+      ],
+      optionCancelIndex: 4,
+      optionDestructiveIndex: 3,
+      clicked: 0
+    };
+  },
+  methods: {
+    handleBtnPress: function() {
+      ActionSheet.show(
+        {
+          options: this.btnOptions,
+          cancelButtonIndex: this.optionCancelIndex,
+          destructiveButtonIndex: this.optionDestructiveIndex,
+          title: "Select An Option"
+        },
+        buttonIndex => {
+          this.clicked = this.btnOptions[buttonIndex];
+        }
+      );
+    }
+  }
+};
+</script>
+
---
+++ src/screens/actionSheet/regular.vue
@@ -0,0 +1,57 @@
+<template>
+  <nb-container :style="{ backgroundColor: '#FBFAFA' }">
+        <nb-header>
+          <nb-left>
+            <nb-button
+              transparent
+              :onPress="() => this.props.navigation.goBack()"
+            >
+              <nb-icon name="arrow-back" />
+            </nb-button>
+          </nb-left>
+          <nb-body>
+            <nb-title>ActionSheet</nb-title>
+          </nb-body>
+          <nb-right />
+        </nb-header>
+        <nb-content padder>
+          <nb-button
+            :onPress="handleBtnPress"
+          >
+            <nb-text>Actionsheet</nb-text>
+          </nb-button>
+        </nb-content>
+      </nb-container>
+</template>
+
+
+<script>
+import { ActionSheet } from "native-base";
+
+export default {
+  data: function() {
+    return {
+      btnOptions: ["Option 0", "Option 1", "Option 2", "Delete", "Cancel"],
+      optionCancelIndex: 4,
+      optionDestructiveIndex: 3,
+      clicked: 0
+    };
+  },
+  methods: {
+    handleBtnPress: function() {
+      ActionSheet.show(
+        {
+          options: this.btnOptions,
+          cancelButtonIndex: this.optionCancelIndex,
+          destructiveButtonIndex: this.optionDestructiveIndex,
+          title: "Select An Option"
+        },
+        buttonIndex => {
+          this.clicked = this.btnOptions[buttonIndex];
+        }
+      );
+    }
+  }
+};
+</script>
+
