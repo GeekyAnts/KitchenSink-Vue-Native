1ce72e1586c84f60e08a2d002b8a9f6921e80412
gauravprwl14 <gauravprwl14@gmail.com>
2018-04-19 15:38:43 +0530

    added actionsheet for the android

--- src/screens/actionSheet/index.android.vue
+++ src/screens/actionSheet/index.android.vue
@@ -1,3 +1,57 @@
 <template>
-  <nb-text>  Action Sheet For Android </nb-text>
+  <nb-container :style="{ backgroundColor: '#fff' }">
+        <nb-header>
+          <nb-left>
+            <nb-button
+              transparent
+              :onPress="() => this.props.navigation.navigate('DrawerOpen')"
+            >
+              <nb-icon name="menu" />
+            </nb-button>
+          </nb-left>
+          <nb-body>
+            <nb-title>ActionSheet</nb-title>
+          </nb-body>
+          <nb-right />
+        </nb-header>
+        <nb-content padder>
+            <nb-list>
+                <nb-list-item
+                    v-for="listItem in listItemArr"
+                    button
+                    :key="listItem.route"
+                    :onPress="() => this.props.navigation.navigate(listItem.route)"
+                >
+                    <nb-left>
+                        <nb-text>
+                            {{listItem.text}}
+                        </nb-text>
+                    </nb-left>
+                    <nb-right>
+                        <nb-icon name="arrow-forward" :style="{ color: '#999' }" />
+                    </nb-right>
+            </nb-list-item>
+            </nb-list>
+        </nb-content>
+      </nb-container>
 </template>
+
+
+<script>
+export default {
+  data: function() {
+    return {
+      listItemArr: [
+        {
+          route: "RegularActionSheet",
+          text: "Regular"
+        },
+        {
+          route: "IconActionSheet",
+          text: "Icon ActionSheet"
+        }
+      ]
+    };
+  }
+};
+</script>
