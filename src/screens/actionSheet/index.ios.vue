10ce49b3c5551c456b9a0e5a67f4e5d8451f4569
gauravprwl14 <gauravprwl14@gmail.com>
2018-04-19 15:10:19 +0530

    added UI for the ios action-sheet

--- src/screens/actionSheet/index.ios.vue
+++ src/screens/actionSheet/index.ios.vue
@@ -1,3 +1,64 @@
 <template>
-  <nb-text>  Action Sheet For IOS </nb-text>
+  <nb-container :style="{ backgroundColor: '#fff' }">
+        <nb-header>
+          <nb-left>
+            <nb-button
+              transparent
+              :onPress="() => this.props.navigation.navigate('DrawerOpen')"
+            >
+              <nb-icon name="menu" />
+            </nb-button>
+          </nb-left>
+          <nb-body>
+            <nb-title>ActionSheet</nb-title>
+          </nb-body>
+          <nb-right />
+        </nb-header>
+        <nb-content padder>
+          <nb-button
+            :onPress="handleBtnPress"
+          >
+            <nb-text>Actionsheet</nb-text>
+          </nb-button>
+        </nb-content>
+      </nb-container>
 </template>
+
+
+<script>
+import { ActionSheet } from "native-base";
+
+export default {
+  data: function() {
+    return {
+      btnOptions: ["Option 0", "Option 1", "Option 2", "Delete", "Cancel"],
+      optionCancelIndex: 4,
+      optionDestructiveIndex: 3,
+      clicked: 0
+    };
+  },
+  //   beforeUpdated: function() {
+  //     console.log("beforeUpdated lifeCycle Method called", this.clicked);
+  //   },
+  //   updated: function() {
+  //     console.log("this. clicked option value", this.clicked);
+  //   },
+  methods: {
+    handleBtnPress: function() {
+      ActionSheet.show(
+        {
+          options: this.btnOptions,
+          cancelButtonIndex: this.optionCancelIndex,
+          destructiveButtonIndex: this.optionDestructiveIndex,
+          title: "Select An Option"
+        },
+        buttonIndex => {
+          this.clicked = this.btnOptions[buttonIndex];
+          //   this.optionCancelIndex = this.clicked;
+        }
+      );
+    }
+  }
+};
+</script>
+
