0a06b4ad34dd87dfb45e50812c1fea858146e9d4
gauravprwl14 <gauravprwl14@gmail.com>
2018-04-24 15:58:15 +0530

    fix title in outline, rounded and transparent button Screen

--- src/screens/button/outlineButton.vue
+++ src/screens/button/outlineButton.vue
@@ -10,7 +10,7 @@
             </nb-button>
           </nb-left>
           <nb-body>
-            <nb-title>Block</nb-title>
+            <nb-title>Outline Button</nb-title>
           </nb-body>
           <nb-right />
         </nb-header>
